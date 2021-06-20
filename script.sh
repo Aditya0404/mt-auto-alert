bad5530b56d6b6497e3cfa5e00f5d1a7a794308d
Aditya Munjal <adityamunjal73@gmail.com>
2021-06-20 09:18:36 +0530

    adding all regions in the script

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ read region
 case $region in
 
   ALL|all|All)
-    declare -a arr=("us-east-1" "ap-south-1" )
+    declare -a arr=("ap-northeast-1" "ap-northeast-2" "ap-northeast-3" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-north-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2")
     for i in "${arr[@]}"
     do
 
