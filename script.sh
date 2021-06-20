a97dfe7721e22a2f876b3eb5f2b190b698247dad
Aditya Munjal <adityamunjal73@gmail.com>
2021-06-20 08:40:57 +0530

    changed the script to ask for region name

--- script.sh
+++ script.sh
@@ -1,32 +1,71 @@
 #!/bin/bash
 
-declare -a arr=("us-east-1" "ap-south-1" )
 
-for i in "${arr[@]}"
-do
+echo "Enter the region in which you want to deploy this Solution or enter All to deploy in all regions: "  
+read region
 
-sed "s/folder/$i/g" template/main.tf > main.tf
+case $region in
 
-TERRAFORM_INIT=$(expect -c "
-spawn terraform init
-expect \"Enter a value\"
-send \"no\r\"
-set timeout 15
-expect eof
-")
+  ALL|all|All)
+    declare -a arr=("us-east-1" "ap-south-1" )
+    for i in "${arr[@]}"
+    do
 
-TERRAFORM_RUN=$(expect -c "
-spawn terraform apply
-expect \"Enter a value\"
-send \"$i\r\"
-set timeout 10
-expect \"Enter a value\"
-send \"yes\r\"
-set timeout 100
-expect eof
-")
+    sed "s/folder/$i/g" template/main.tf > main.tf
 
-echo "$TERRAFORM_INIT"
-echo "$TERRAFORM_RUN"
+    TERRAFORM_INIT=$(expect -c "
+    spawn terraform init
+    expect \"Enter a value\"
+    send \"no\r\"
+    set timeout 15
+    expect eof
+    ")
 
-done
+    TERRAFORM_RUN=$(expect -c "
+    spawn terraform apply
+    expect \"Enter a value\"
+    send \"$i\r\"
+    set timeout 10
+    expect \"Enter a value\"
+    send \"yes\r\"
+    set timeout 100
+    expect eof
+    ")
+
+    echo "$TERRAFORM_INIT"
+    echo "$TERRAFORM_RUN"
+
+    done
+
+    ;;
+ 
+
+
+  *)
+    sed "s/folder/$region/g" template/main.tf > main.tf
+    TERRAFORM_INIT=$(expect -c "
+    spawn terraform init
+    expect \"Enter a value\"
+    send \"no\r\"
+    set timeout 15
+    expect eof
+    ")
+
+    TERRAFORM_RUN=$(expect -c "
+    spawn terraform apply
+    expect \"Enter a value\"
+    send \"$region\r\"
+    set timeout 20
+    expect \"Enter a value\"
+    send \"yes\r\"
+    set timeout 100
+    expect eof
+    ")
+
+   echo "$TERRAFORM_INIT"
+   echo "$TERRAFORM_RUN"
+    ;;
+
+
+esac
+
